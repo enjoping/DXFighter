fe438ca3f537b634b9f551afe3657fdcb27cb32a
Joe Pietler <pietler@brightsolutions.de>
2016-12-30 20:48:48 +0100

    Add the Endblk Class which is part of the blocks section and is used by the Block class to signalize the end of a block.

---
+++ lib/Endblk.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jpietler
+ * Date: 30.12.16
+ * Time: 16:18
+ *
+ * Dokumentation http://www.autodesk.com/techpubs/autocad/acad2000/dxf/endblk_dxf_05.htm
+ */
+
+namespace DXFighter\lib;
+
+/**
+ * Class BlockRecord
+ * @package DXFighter\lib
+ */
+class Endblk extends Entity {
+
+  function __construct($layer, $pointer) {
+    $this->entityType = 'endblk';
+    $this->layer = $layer;
+    $this->pointer = $pointer;
+    parent::__construct();
+  }
+
+  public function render() {
+    $output = parent::render();
+    array_push($output, 100, 'AcDbBlockEnd');
+    return implode($output, PHP_EOL);
+  }
+}
