1f300e2928d7dbc3fc9384dd345d2f4acb9d3434
Joe Pietler <pietler@brightsolutions.de>
2016-12-30 20:47:57 +0100

    Add the Block Class which are part of the blocks section.

---
+++ lib/Block.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jpietler
+ * Date: 30.12.16
+ * Time: 16:18
+ *
+ * Dokumentation http://www.autodesk.com/techpubs/autocad/acad2000/dxf/block_dxf_05.htm
+ */
+
+namespace DXFighter\lib;
+
+/**
+ * Class BlockRecord
+ * @package DXFighter\lib
+ */
+class Block extends Entity {
+  protected $name;
+  protected $xrefPath;
+  protected $endblk;
+  protected $point = array(0, 0, 0);
+
+  function __construct($name) {
+    $this->entityType = 'block';
+    $this->name = $name;
+    $this->flags = array_fill(0, 6, 0);
+    $this->pointer = $this->getUniqueID();
+    parent::__construct();
+    $this->endblk = new Endblk($this->layer, $this->pointer);
+  }
+
+  public function render() {
+    $output = parent::render();
+    array_push($output, 100, 'AcDbBlockBegin');
+    array_push($output, 2, strtoupper($this->name));
+    array_push($output, 70, $this->flagsToString());
+    array_push($output, $this->point($this->point));
+    array_push($output, 3, strtoupper($this->name));
+    array_push($output, 1, $this->xrefPath);
+    array_push($output, $this->endblk->render());
+    return implode($output, PHP_EOL);
+  }
+}
