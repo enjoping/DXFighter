e86dd25406a3c21fea7e43e87d67afd1ef5628a4
Joe Pietler <pietler@brightsolutions.de>
2017-01-11 13:17:58 +0100

    Add the polyline entity type which is used to draw lines between several points.

---
+++ lib/Polyline.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jpietler
+ * Date: 30.12.16
+ * Time: 16:18
+ *
+ * Dokumentation http://www.autodesk.com/techpubs/autocad/acad2000/dxf/polyline_dxf_06.htm
+ */
+
+namespace DXFighter\lib;
+
+/**
+ * Class Polyline
+ * @package DXFighter\lib
+ */
+class Polyline extends Entity {
+  protected $base = array(0, 0, 0);
+  protected $thickness;
+  protected $extrusion;
+  protected $points = array();
+  protected $dimension;
+  protected $seqend;
+
+  function __construct($dimension = 2) {
+    $this->entityType = 'polyline';
+    $this->flags = array_fill(0, 7, 0);
+    $this->dimension = $dimension;
+    parent::__construct();
+  }
+
+  public function addPoint($point) {
+    $this->points[] = new Vertex($point, $this->dimension, $this->handle, $this->layer);
+  }
+
+  private function addSeqend() {
+    $this->seqend = new Seqend($this->handle, $this->layer);
+  }
+
+  public function render() {
+    if(!isset($this->seqend)) {
+      $this->addSeqend();
+    }
+    $output = parent::render();
+    array_push($output, 100, 'AcDb'.$this->dimension.'dPolyline');
+    array_push($output, $this->point($this->base));
+    array_push($output, 70, $this->flagsToString());
+
+    foreach($this->points as $point) {
+      array_push($output, $point->render());
+    }
+
+    array_push($output, $this->seqend->render());
+    return implode($output, PHP_EOL);
+  }
+}
