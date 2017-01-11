eb562b3ea1d56342b4b84c0a4ae5b1d6c6f868fc
Joe Pietler <pietler@brightsolutions.de>
2017-01-11 15:10:07 +0100

    Add the Seqend entity type which shows the end of a polyline

---
+++ lib/Seqend.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jpietler
+ * Date: 30.12.16
+ * Time: 16:18
+ *
+ * Dokumentation http://www.autodesk.com/techpubs/autocad/acad2000/dxf/seqend_dxf_06.htm
+ */
+
+namespace DXFighter\lib;
+
+/**
+ * Class Polyline
+ * @package DXFighter\lib
+ */
+class Seqend extends Entity {
+  protected $base = array(0, 0, 0);
+  protected $thickness;
+  protected $extrusion;
+  protected $points = array();
+  protected $dimension;
+
+  function __construct($pointer, $layer) {
+    $this->entityType = 'seqend';
+    $this->pointer = $pointer;
+    parent::__construct();
+  }
+
+  public function render() {
+    $output = parent::render();
+    return implode($output, PHP_EOL);
+  }
+}
