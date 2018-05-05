5ebc289c41c1b82b03a54d4d8cd08609701a7f5c
Joe Pietler <pietler@brightsolutions.de>
2018-05-05 08:51:52 +0200

    Load the id of the tested object instead of assuming it in the tests so that we don't need to change all tests when adding an object.

--- tests/TextTest.php
+++ tests/TextTest.php
@@ -17,9 +17,14 @@ final class TextTest extends TestCase {
       Text::class,
       $text
     );
+
+    $id = $text->getHandle();
+    $this->assertInternalType("int", hexdec($id));
+    $this->assertGreaterThan(0, hexdec($id));
+
     $this->assertEquals(
       $text->render(),
-      "0\nTEXT\n5\n1E\n330\n0\n100\nAcDbEntity\n67\n0\n8\n0\n6\nBYLAYER\n62\n256\n48\n1\n60\n0\n100\nAcDbText\n39\n2\n10\n-10.000\n20\n90.000\n30\n0.000\n40\n50\n1\ntestText\n50\n2\n41\n1\n7\nSTANDARD\n72\n0\n100\nAcDbText\n73\n0"
+      "0\nTEXT\n5\n".$id."\n330\n0\n100\nAcDbEntity\n67\n0\n8\n0\n6\nBYLAYER\n62\n256\n48\n1\n60\n0\n100\nAcDbText\n39\n2\n10\n-10.000\n20\n90.000\n30\n0.000\n40\n50\n1\ntestText\n50\n2\n41\n1\n7\nSTANDARD\n72\n0\n100\nAcDbText\n73\n0"
     );
 
     $text->setHorizontalJustification(1);
@@ -27,7 +32,7 @@ final class TextTest extends TestCase {
 
     $this->assertEquals(
       $text->render(),
-      "0\nTEXT\n5\n1E\n330\n0\n100\nAcDbEntity\n67\n0\n8\n0\n6\nBYLAYER\n62\n256\n48\n1\n60\n0\n100\nAcDbText\n39\n2\n10\n-10.000\n20\n90.000\n30\n0.000\n40\n50\n1\ntestText\n50\n2\n41\n1\n7\nSTANDARD\n72\n1\n100\nAcDbText\n73\n2"
+      "0\nTEXT\n5\n".$id."\n330\n0\n100\nAcDbEntity\n67\n0\n8\n0\n6\nBYLAYER\n62\n256\n48\n1\n60\n0\n100\nAcDbText\n39\n2\n10\n-10.000\n20\n90.000\n30\n0.000\n40\n50\n1\ntestText\n50\n2\n41\n1\n7\nSTANDARD\n72\n1\n100\nAcDbText\n73\n2"
     );
   }
 }
